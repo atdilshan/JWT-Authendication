21ac0a1e74e3c4df0243cfbe04e819f27ab5f961
atdilshan <atdilshan0127@gmail.com>
2022-07-22 15:00:57 +0530

    Logout Created

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,13 +7,16 @@ const {hashValidator} = require("../helpers/hashing");
 const {tokenGenerator} = require("../helpers/token");
 const authVerify = require("../helpers/authVerify");
 
+// User signup http request 
 authRoutes.post("/signup", async (req, res) => {
     try{
         const hashPassword = await hashGenerate(req.body.password);
         const user = new User({
             username:req.body.username,
             email:req.body.email,
-            password:hashPassword
+            password:hashPassword,
+            role:"0",
+            statuses:"1" // statuses=0(blocked), statuses=1(active)
         });
         const savedUser = await user.save();
         res.send(savedUser);
@@ -22,6 +25,45 @@ authRoutes.post("/signup", async (req, res) => {
     }
 });
 
+// Vehicle owner register http request 
+authRoutes.post("/register", async (req, res) => {
+    try{
+        const hashPassword = await hashGenerate(req.body.password);
+        const user = new User({
+            username:req.body.username,
+            email:req.body.email,
+            password:hashPassword,
+            role:"1",
+            statuses:"2" // statuses=0(blocked), statuses=1(active), statuses=2(pending)
+        });
+        const savedUser = await user.save();
+        res.send(savedUser);
+    } catch (error){
+        res.send(error)
+    }
+});
+
+
+// Admin register http request
+// this is for super admin
+authRoutes.post("/admin-register", async (req, res) => {
+    try{
+        const hashPassword = await hashGenerate(req.body.password);
+        const user = new User({
+            username:req.body.username,
+            email:req.body.email,
+            password:hashPassword,
+            role:"2",
+            statuses:"2" // statuses=0(blocked), statuses=1(active), statuses=2(pending for accepted)
+        });
+        const savedUser = await user.save();
+        res.send(savedUser);
+    } catch (error){
+        res.send(error)
+    }
+});
+
+// User, vehicle owner, admin signin http request
 authRoutes.post("/signin", async (req, res) => {
     try{
         const existingUser = await User.findOne({email:req.body.email});
@@ -32,10 +74,15 @@ authRoutes.post("/signin", async (req, res) => {
             if(!checkUser){
                 res.send("Password is invalid");
             } else {
-                // res.send("Login success!");
                 const token = await tokenGenerator(existingUser.email);
                 res.cookie("jwt", token, {httpOnly:true})
-                res.send(token);
+                if(existingUser.role == 0){
+                    res.send(`User Login successful! Your Token is ` + token);
+                }else if(existingUser.role == 1){
+                    res.send(`Vehicle Owner Login successful! Your Token is ` + token);
+                }else{
+                    res.send(`Admin Login successful! Your Token is ` + token);
+                }
             }
         }        
     } catch (error) {
@@ -43,8 +90,16 @@ authRoutes.post("/signin", async (req, res) => {
     }
 })
 
+// check protected route
 authRoutes.get("/protected", authVerify, (req, res) => {
     res.send("I am protected route")
 })
+
+// logout http request
+authRoutes.get("/logout", (req, res) => {
+    res.clearCookie('jwt', {httpOnly: true});
+    res.send('Logout Successfull!');
+})
+
 
 module.exports = authRoutes;
